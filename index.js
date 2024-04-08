c6a8b6c8dbd34b0d21781df071940a8fa5879b16
Vikas Gola <vikasgola2015@gmail.com>
2024-04-08 20:45:39 +0530

    load video with captions after submitting form

--- index.js
+++ index.js
@@ -2,5 +2,23 @@ var captionForm = document.getElementById("captionForm");
 
 captionForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    alert("form submitted!");
+
+    // get all the submitted values in the form
+    const videoUrl = document.getElementById("videoUrl").value;
+    const startTimestamp = document.getElementById("startTimestamp").value;
+    const endTimestamp = document.getElementById("endTimestamp").value;
+    const caption = document.getElementById("caption").value;
+
+    // set video url to video element
+    const video = document.getElementById("videoPlayer");
+    video.src = videoUrl;
+
+    // Check if a TextTrack already exists, if not, create one
+    if (!video.textTracks[0]) {
+        video.addTextTrack("captions");
+    }
+
+    // Add cue to the TextTrack
+    const track = video.textTracks[0];
+    track.addCue(new VTTCue(startTimestamp, endTimestamp, caption));
 });
