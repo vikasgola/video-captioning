cff9578487ac5ebcbfde566d0021de7caa7c1262
Vikas Gola <vikasgola2015@gmail.com>
2024-04-08 23:32:54 +0530

    auto collapse all other caption section on opening or adding new caption

--- index.js
+++ index.js
@@ -9,29 +9,39 @@ function addNewCaptionForm(){
     captionCount++;
 
     const captionsAccordion = document.getElementById("captionsAccordion");
+
+    // close all existing accordion items
+    const existingItems = captionsAccordion.querySelectorAll(".accordion-item");
+    existingItems.forEach(item => {
+        const collapseElement = item.querySelector(".accordion-collapse");
+        const collapseInstance = new bootstrap.Collapse(collapseElement, { toggle: false });
+        collapseInstance.hide();
+    });
+
+    // create new accordion item
     const captionInput = document.createElement("div");
     captionInput.classList.add("accordion-item");
 
     captionInput.innerHTML = `
         <h2 class="accordion-header" id="captionHeading${captionCount}">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#captionCollapse${captionCount}" aria-expanded="false" aria-controls="captionCollapse${captionCount}">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#captionCollapse${captionCount}" aria-expanded="true" aria-controls="captionCollapse${captionCount}">
                 Caption ${captionCount}
             </button>
         </h2>
-        <div id="captionCollapse${captionCount}" class="accordion-collapse collapse" aria-labelledby="captionHeading${captionCount}" data-bs-parent="#captionsAccordion">
+        <div id="captionCollapse${captionCount}" class="accordion-collapse collapse show" aria-labelledby="captionHeading${captionCount}" data-bs-parent="#captionsAccordion">
             <div class="accordion-body">
                 <div class="row mb-3">
-            <div class="col">
-                <label for="startTimestamp${captionCount}" class="form-label">Start Timestamp:</label>
-                <input type="text" class="form-control" id="startTimestamp${captionCount}" name="startTimestamp${captionCount}" required>
-            </div>
-            <div class="col">
-                <label for="endTimestamp${captionCount}" class="form-label">End Timestamp:</label>
-                <input type="text" class="form-control" id="endTimestamp${captionCount}" name="endTimestamp${captionCount}" required>
-            </div>
-        </div>
-        <label for="caption${captionCount}" class="form-label">Caption:</label>
-        <textarea class="form-control" id="caption${captionCount}" name="caption${captionCount}" rows="4" required></textarea>
+                    <div class="col">
+                        <label for="startTimestamp${captionCount}" class="form-label">Start Timestamp:</label>
+                        <input type="text" class="form-control" id="startTimestamp${captionCount}" name="startTimestamp${captionCount}" required>
+                    </div>
+                    <div class="col">
+                        <label for="endTimestamp${captionCount}" class="form-label">End Timestamp:</label>
+                        <input type="text" class="form-control" id="endTimestamp${captionCount}" name="endTimestamp${captionCount}" required>
+                    </div>
+                </div>
+                <label for="caption${captionCount}" class="form-label">Caption:</label>
+                <textarea class="form-control" id="caption${captionCount}" name="caption${captionCount}" rows="4" required></textarea>
             </div>
         </div>
     `;
